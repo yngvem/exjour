81bd8c14604bb4f6286e76a8310108fc2e81c055
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-08-31 10:47:21 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,32 @@
+======
+exjour
+======
+
+
+.. image:: https://readthedocs.org/projects/exjour/badge/?version=latest
+        :target: https://exjour.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
+
+
+Journal for numerical experiments
+
+
+* Free software: MIT license
+* Documentation: https://exjour.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ src/exjour/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Yngve Mardal Moe"""
+__email__ = 'yngve.m.moe@gmail.com'
+__version__ = '0.0.1'
+
+
+from pathlib import Path
+import sys
+
+from git import Repo
+
+
+class Journal:
+    def __init__(self, directory):
+        self.repo = Repo(directory)
+        self.working_dir = Path(self.repo.working_dir)
+    
+    def log_experiment(self, name, hash_path):
+        if len(self.repo.index.diff("HEAD")) != 0:
+            raise ValueError("Staging area must be empty before running experiment")
+
+        untracked_files = {untracked_file for untracked_file in self.repo.untracked_files}
+        changed_files = {diff.a_path for diff in self.repo.index.diff(None)}
+        
+        if len(untracked_files) == 0 and len(changed_files) == 0:
+            return
+
+        self.repo.index.add(untracked_files | changed_files)
+        self.repo.index.commit(f"AUTOCOMMIT FOR EXPERIMENT: {name}")
+
+        try:
+            with open(hash_path, "w") as f:
+                f.write(self.repo.head.commit.hexsha)
+        except Exception as e:
+            self.repo.head.reset("HEAD~")
+            raise e
+        else:
+            self.repo.head.reset("HEAD~")
+        
